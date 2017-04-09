ab63f2dd1f8ef9e08fc1aba649e78a9411bab521
Sergey Belyakov <mr.belyakov@hotmail.com>
2017-04-09 22:25:28 +0700

    first step

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergey
+ * Date: 08.04.17
+ * Time: 21:35
+ */
+
+$step = 16;
+
+echo "Hello World!!!";
+echo "<br>";
+echo "Play on Guitar to the MTV!";
+$fact=0;
+for ($i=1; $i<=$step; $i++)
+{
+    $fact = 1;
+    for ($j=1; $j<=$i; $j++)
+    {
+        $fact = $fact*$j;
+
+    }
+    echo "<br>";
+    echo $fact;
+}
+
+
+?>
